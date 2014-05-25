e543f5df28f1091ea58b6b96cccb740d2703967b
continueing <continueing@gmail.com>
2014-05-25 19:43:42 +0900

    rearrange the sections in the class detail info page.

--- src/main/java/com/blackpigstudio/classweek/main/ui/menu/home/class_detail_info/review/ReviewDialog.java
+++ src/main/java/com/blackpigstudio/classweek/main/ui/menu/home/class_detail_info/review/ReviewDialog.java
@@ -4,15 +4,20 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import com.blackpigstudio.classweek.R;
 import com.blackpigstudio.classweek.main.domain.class_info.Review;
+import com.blackpigstudio.classweek.main.module.listview.ProgressbarFooter;
 
 /**
  * Created by continueing on 2014. 5. 22..
  */
 public class ReviewDialog extends Dialog {
+    private ProgressbarFooter progressbarFooter;
+    private ListView lv_reviews;
+    private ArrayAdapterForReviewListView arrayAdapterForReviewListView;
 
     public ReviewDialog(Context context) {
         super(context);
@@ -25,11 +30,48 @@ public class ReviewDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_reviews);
 
-        ListView listView = (ListView)findViewById(R.id.lv_dialog_reviews);
-        ArrayAdapterForReviewListView arrayAdapterForReviewListView = new ArrayAdapterForReviewListView(getContext(),R.layout.item_review);
-        listView.setAdapter(arrayAdapterForReviewListView);
+        lv_reviews = (ListView)findViewById(R.id.lv_dialog_reviews);
+        progressbarFooter = new ProgressbarFooter(lv_reviews, getLayoutInflater());
+        progressbarFooter.setVisibility(true);
+        arrayAdapterForReviewListView = new ArrayAdapterForReviewListView(getContext(), R.layout.item_review);
+        lv_reviews.setAdapter(arrayAdapterForReviewListView);
+
         arrayAdapterForReviewListView.add(new Review());
         arrayAdapterForReviewListView.add(new Review());
         arrayAdapterForReviewListView.add(new Review());
+
+        setEvent();
     }
+
+    public void setEvent()
+    {
+        lv_reviews.setOnScrollListener(new AbsListView.OnScrollListener() {
+            private boolean visibilityOfLastItem = false;
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int aScrollState) {
+                if(aScrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE)
+                {
+                    if(aScrollState== AbsListView.OnScrollListener.SCROLL_STATE_IDLE && visibilityOfLastItem)
+                    {
+                        // end of item call request to server
+
+                        arrayAdapterForReviewListView.add(new Review());
+                        arrayAdapterForReviewListView.add(new Review());
+                        arrayAdapterForReviewListView.add(new Review());
+                        arrayAdapterForReviewListView.add(new Review());
+                        arrayAdapterForReviewListView.add(new Review());
+                        arrayAdapterForReviewListView.add(new Review());
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int aFirstVisibleItem, int aVisibleItemCount, int aTotalItemCount)
+            {
+                visibilityOfLastItem = aFirstVisibleItem + aVisibleItemCount >= aTotalItemCount;
+            }
+        });
+    }
+
+
 }
